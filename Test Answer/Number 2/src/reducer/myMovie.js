592f31296919eb8aa039f9a01c7b115748921315
bapuks <49339986+bapuks@users.noreply.github.com>
2021-05-22 17:22:44 +0700

    Add files via upload

--- Test Answer/Number 2/src/reducer/myMovie.js
+++ Test Answer/Number 2/src/reducer/myMovie.js
@@ -1,1 +1,23 @@
+import { MYMOVIE } from '../actions/ActionTypes';
 
+const initialState = {
+	myMovie: [],
+};
+
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case MYMOVIE.FETCH_SUCCESS:
+      localStorage.setItem('myMovie', [...state.myMovie, action.payload]);
+      return {
+        ...state,
+        myMovie: [...state.myMovie, action.payload]
+      };
+		case MYMOVIE.REMOVE_MOVIE:
+			return {
+				...state,
+				myMovie: [...state.myMovie.filter((item, index) => index !== action.payload)]
+			};
+     default:
+      return state;
+  }
+};
